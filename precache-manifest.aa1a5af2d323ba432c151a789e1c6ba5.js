1c0e10d7cedb44a0c388fa2314ce6285078fea16
Johnkiran <johnkiran.g@alphaclinicalsystems.com>
2019-03-28 14:59:45 +0530

    Updates

---
+++ precache-manifest.aa1a5af2d323ba432c151a789e1c6ba5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "cf9bf42a41dff5b3c05b",
+    "url": "./static/js/main.cf9bf42a.chunk.js"
+  },
+  {
+    "revision": "32a4d7df7c1551aef447",
+    "url": "./static/js/1.32a4d7df.chunk.js"
+  },
+  {
+    "revision": "cf9bf42a41dff5b3c05b",
+    "url": "./static/css/main.21c2e67d.chunk.css"
+  },
+  {
+    "revision": "32a4d7df7c1551aef447",
+    "url": "./static/css/1.e5a0b821.chunk.css"
+  },
+  {
+    "revision": "0accaab9ca7fb1f2d0db0086a5b9f09d",
+    "url": "./index.html"
+  }
+];
